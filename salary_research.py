404f85a1d3ad23adb7ecb8ca6703fc09ad86403b
iCherniakov <3699818@mail.ru>
2020-05-27 02:21:17 +0300

    Update salary_research.py

--- salary_research.py
+++ salary_research.py
@@ -27,21 +27,21 @@ def predict_rub_salary_hh(job_title):
                'period': 30}
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    response_data = response.json()
+    vacancies_search_response = response.json()
 
 
-    for page in range(response_data['pages'] + 1):
+    for page in range(vacancies_search_response['pages'] + 1):
         payload = {'text': job_title,
                    'area': 1,
                    'period': 30,
                    'page': page}
         response = requests.get(url, params=payload)
         response.raise_for_status()
-        response_data = response.json()
+        vacancies_search_response = response.json()
 
-        vacancies.extend(response_data['items'])
+        vacancies.extend(vacancies_search_response['items'])
 
-    vacancies_found = response_data['found']
+    vacancies_found = vacancies_search_response['found']
     average_salaries = []
     vacancies_processed = 0
     salaries_sum = 0
@@ -71,7 +71,7 @@ def predict_rub_salary_hh(job_title):
 
 def predict_rub_salary_sj(job_title):
     url = 'https://api.superjob.ru/2.0/vacancies'
-    headers = {'X-Api-App-Id': SJOB_TOKEN}
+    headers = {'X-Api-App-Id': sjob_token}
     payrole = {
         'catalogues': 48,
         'town': 4,
@@ -80,10 +80,10 @@ def predict_rub_salary_sj(job_title):
     }
     response = requests.get(url, headers=headers, params=payrole)
     response.raise_for_status()
-    response_data = response.json()
+    vacancies_search_response = response.json()
 
     vacancies = []
-    vacancies_found = response_data['total']
+    vacancies_found = vacancies_search_response['total']
     page_amount = math.ceil(vacancies_found / 20)
 
     for page in range(page_amount + 1):
@@ -96,8 +96,8 @@ def predict_rub_salary_sj(job_title):
         }
         response = requests.get(url, headers=headers, params=payrole)
         response.raise_for_status()
-        response_data = response.json()
-        vacancies.extend(response_data['objects'])
+        vacancies_search_response = response.json()
+        vacancies.extend(vacancies_search_response['objects'])
 
     average_salaries = []
     vacancies_processed = 0
@@ -123,29 +123,29 @@ def predict_rub_salary_sj(job_title):
     return vacancies_found, vacancies_processed, average_salary
 
 
-def fill_salary_data(func):
-    salary_data = {}
+def fill_salary_specs(func):
+    salary_for_languages = {}
     for language in languages:
         job_title = 'Программист ' + language
-        vacancy_data = func(job_title)
-        salary_for_language = {'vacancies_found': vacancy_data[0],
-                               'vacancies_processed': vacancy_data[1],
-                               'average_salary': vacancy_data[2]
+        vacancy_specs = func(job_title)
+        salary_for_language = {'vacancies_found': vacancy_specs[0],
+                               'vacancies_processed': vacancy_specs[1],
+                               'average_salary': vacancy_specs[2]
                                }
-        salary_data[language] = salary_for_language
-    return salary_data
+        salary_for_languages[language] = salary_for_language
+    return salary_for_languages
 
 
-def draw_ascii_table(salary_for_language_data, title):
-    salary_table_data = [('Язык программирования', 'Вакансий найдено',
+def draw_ascii_table(salary_for_language, title):
+    salary_table = [('Язык программирования', 'Вакансий найдено',
                           'Вакансий обработано', 'Средняя зарплата')]
-    for language in salary_for_language_data:
-        data_for_table = (language, salary_for_language_data[language]['vacancies_found'],
-                          salary_for_language_data[language]['vacancies_processed'],
-                          salary_for_language_data[language]['average_salary'])
-        salary_table_data.append(data_for_table)
+    for language in salary_for_language:
+        specs_for_table = (language, salary_for_language[language]['vacancies_found'],
+                          salary_for_language[language]['vacancies_processed'],
+                          salary_for_language[language]['average_salary'])
+        salary_table.append(specs_for_table)
 
-    table_instance = AsciiTable(salary_table_data, title)
+    table_instance = AsciiTable(salary_table, title)
     table_instance.justify_columns[2] = 'right'
     print(table_instance.table)
     print()
@@ -153,13 +153,13 @@ def draw_ascii_table(salary_for_language_data, title):
 
 if __name__ == '__main__':
     load_dotenv()
-    SJOB_TOKEN = os.getenv('SJOB_TOKEN')
+    sjob_token = os.getenv('SJOB_TOKEN')
 
     languages = ['Python', 'JavaScript', 'Java', 'C++',
                  'Ruby', 'PHP', 'C#', 'C', 'TypeScript', 'Go']
 
-    salary_for_languages_sj = fill_salary_data(predict_rub_salary_sj)
-    salary_for_languages_hh = fill_salary_data(predict_rub_salary_hh)
+    salary_for_languages_sj = fill_salary_specs(predict_rub_salary_sj)
+    salary_for_languages_hh = fill_salary_specs(predict_rub_salary_hh)
 
     draw_ascii_table(salary_for_languages_hh, 'HeadHunter, Moscow')
     draw_ascii_table(salary_for_languages_sj, 'SuperJob, Moscow')
